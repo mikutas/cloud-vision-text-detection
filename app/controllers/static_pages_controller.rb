19db07cb1029c7b6b76cc3df6349573559b06554
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2018-05-06 10:07:55 +0900

    [add]Cloud Vision APIを試しに動かしてみる処理を追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,23 @@
 class StaticPagesController < ApplicationController
+  # Imports the Google Cloud client library
+  require "google/cloud/vision"
+
   def input
   end
 
   def result
+    # Your Google Cloud Platform project ID
+    project_id = "my-mojiokoshi"
+
+    # Instantiates a client
+    vision = Google::Cloud::Vision.new project: project_id
+
+    # The name of the image file to annotate
+    file_name = "app/assets/images/test.png"
+
+    # Performs label detection on the image file
+    @document = vision.image(file_name).document
+
+    puts @document.text
   end
 end
